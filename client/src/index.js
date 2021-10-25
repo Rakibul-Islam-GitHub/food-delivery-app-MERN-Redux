9d76db250d25804718cf5af341909813ede8ff9e
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-10-26 00:24:42 +0600

    redux store added

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
